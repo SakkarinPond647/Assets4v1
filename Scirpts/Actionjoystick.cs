92454b0174eb51572b0154f5a4186d5a17cc12f6
SakkarinPond647 <61303444+SakkarinPond647@users.noreply.github.com>
2020-08-07 16:35:33 +0700

    Update Actionjoystick.cs

--- Scirpts/Actionjoystick.cs
+++ Scirpts/Actionjoystick.cs
@@ -4,42 +4,51 @@ using UnityEngine;
 
 namespace game4v1
 {
-public class Actionjoystick : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+    public class Actionjoystick : MonoBehaviour
     {
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    }
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        void Update()
+        {
 
+        }
+        public void Action()
+        {
+            if (Swaprole.chooserole.Role == "Survival")
+            {
+                if (Player.CanInvest == true)
+                {
+                    Debug.Log("Invest");
+                }
+
+                if (Player.canOpenChest == true)
+                {
+                    Debug.Log("Openbox");
+                    Box.isOpen = true;
+                }
+                    
+            }
+            else
+            {
+                Debug.Log("Attack");
+            }
+        }
+        public void SkillandUseItem()
+        {
+            if (Swaprole.chooserole.Role == "Survival")
+            {
+
+                Debug.Log("Useitem");
+            }
+            else
+            {
+                Debug.Log("Skill");
+            }
+        }
     }
-    public void Action()
-    {
-        if (Swaprole.chooserole.Role == "Survival")
-        {
-            if(Player.CanInvest == true)
-            Debug.Log("Invest");
-        }
-        else
-        {
-            Debug.Log("Attack");
-        }
-    }
-    public void SkillandUseItem()
-    {
-        if (Swaprole.chooserole.Role == "Survival")
-        {
-            
-            Debug.Log("Useitem");
-        }
-        else
-        {
-            Debug.Log("Skill");
-        }
-    }
-}
 }
